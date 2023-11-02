971729d9856965c5eddbfc5f14e89265daecafd1
Sebastian Lueneburg <se-l@gmx.de>
2023-11-02 11:23:42 +0800

    test

---
+++ Tests/Algorithm/CSharp/Core/Foundations.CachedTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace QuantConnect.Tests.Algorithm.CSharp.Core
+{
+    [TestFixture]
+    public class FoundationsTests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
